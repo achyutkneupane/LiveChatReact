f1bc6e824c9fa9ff18908ebec6be06a3d4f2cf55
Achyut Neupane <achyutkneupane@gmail.com>
2024-01-31 10:08:04 +0545

    feat: Messages can be sent

--- src/Utils/fetchers/chatFetcher.ts
+++ src/Utils/fetchers/chatFetcher.ts
@@ -4,7 +4,7 @@ const basePath = "/chatbox";
 
 export const fetchChatBoxes = () => anAuthFetch(basePath, null, "GET");
 export const fetchMessages = (chatBoxId: string) => anAuthFetch(`${basePath}/${chatBoxId}`, null, "GET");
-export const sendMessage = (chatBoxId: string, content: string) => anAuthFetch(`${basePath}/${chatBoxId}`, {content}, "POST");
+export const sendMessage = (chatBoxId: string, content: string) => anAuthFetch(`${basePath}/${chatBoxId}/message`, {content}, "POST");
 export const createNewBox = (receiverId: string) => anAuthFetch(`/chat/${receiverId}`, {}, "POST");
 export const createNewGroupBox = (payload: {
     name: string,
